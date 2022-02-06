db7261d80d9d05e01cd0c9cc849ae74711147554
sa_nk133 <Sunita@277>
2022-02-06 18:42:26 -0500

    final code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import imp
+
 import mglearn as mglearn
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,6 +9,9 @@ from sklearn.neighbors import KDTree, KNeighborsRegressor
 
 
 #using pandas to get mean median mode of each class(Gender)
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
 employee = pd.read_csv('Social_Network_Ads.csv')
 dept_gender_salary =  employee.groupby(['Gender'],as_index=False).EstimatedSalary.mean()
 print(dept_gender_salary)
@@ -20,6 +25,8 @@ dept_gender_age_mode = employee.groupby(['Gender'],as_index=False).Age.describe(
 print(dept_gender_age_mode )
 dept_gender_age_mode_gender = employee.groupby(['Gender'],as_index=False).EstimatedSalary.describe()
 print(dept_gender_age_mode_gender)
+print(employee.mode(numeric_only=True))
+
 
 #Importing The Dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
@@ -51,7 +58,8 @@ X[:,1] = le.fit_transform(X[:,1])
 print(X)
 
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_validate, GridSearchCV
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 print("Training data set")
 print(X_train)
@@ -74,20 +82,20 @@ classifier.fit(X_train, y_train)
 
 
 y_pred = classifier.predict(X_test)
-print("this is testing data result")
-print(y_test)
 print("this is expected data result")
+print(y_test)
+print("this is testing data result")
 print(y_pred)
 
 #print accuracy and confusion matrix
-from sklearn.metrics import confusion_matrix,accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score, mean_squared_error
+
 cm = confusion_matrix(y_test, y_pred)
 ac = accuracy_score(y_test,y_pred)
 print(cm)
 print(ac)
 
 #Kd- tree code
-rng = np.random.RandomState(0)
 tree = KDTree(X, leaf_size=2)
 dist, ind = tree.query(X[:1], k=3)
 print("the three indices for the test data ")
@@ -96,7 +104,7 @@ print("the distnace of the test data")
 print(dist)
 
 #trying to print a graph between accuracy and n each time using a graph
-neighbors = np.arange(1, 9)
+neighbors = np.arange(5, 9)
 train_accuracy = np.empty(len(neighbors))
 test_accuracy = np.empty(len(neighbors))
 
@@ -120,4 +128,24 @@ reg = KNeighborsRegressor(n_neighbors=5)
 reg.fit(X_train,y_train)
 print("Test set R^2: {:.2f}".format(reg.score(X_test, y_test)))
 
+#Ball Tree Algorithm
+from sklearn.neighbors import BallTree
+tree = BallTree(X, leaf_size=2)
+dist, ind = tree.query(X[:1], k=3)
+print(dist,ind)
 
+#using cross-validation for checking Data Leakage Problem
+from sklearn.model_selection import KFold
+kf = KFold(n_splits=2)
+for train, test in kf.split(X):
+ print((train, test))
+
+
+#create a tree using brute force technique
+model_tree= DecisionTreeClassifier(max_leaf_nodes=8,class_weight='balanced')
+model_tree.fit(X_train,y_train)
+
+plt.figure(figsize=(20,10))
+
+plot_tree(model_tree,class_names=["0","1"],rounded=True,filled=True)
+plt.show()
